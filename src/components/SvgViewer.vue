3f763a6b02f4575667d9f45ec4aaf7401c8b687b
Fredrik Vedvik <fredrik@vedvik.tech>
2022-08-12 23:24:51 +0200

    fisk

--- src/components/SvgViewer.vue
+++ src/components/SvgViewer.vue
@@ -1,14 +1,18 @@
 <template>
-    <div id="svg-viewer" v-html="svg"></div>
+    <div id="svg-viewer" v-html="svg" :style="loading ? 'display: none;' : ''"></div>
 </template>
 <script lang="ts" setup>
-import { watch, onMounted } from "vue";
+import { watch, onMounted, ref } from "vue";
 
 const props = defineProps<{
     svg: string;
 }>();
 
-const fixElements = () => {
+const loading = ref(false)
+
+const fixElements = async () => {
+    loading.value = true;
+    await new Promise(r => setTimeout(r, 10))
     const els = document.getElementById("svg-viewer")?.children;
 
     if (!els) return;
@@ -18,12 +22,13 @@ const fixElements = () => {
         el.removeAttribute("width");
         el.removeAttribute("height");
     }
+    loading.value = false;
 };
 
-onMounted(() => setTimeout(fixElements, 10));
+onMounted(fixElements);
 
 watch(
     () => props.svg,
-    () => setTimeout(fixElements, 10)
+    fixElements
 );
 </script>
