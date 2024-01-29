75d0c2c3c39115be0eeec2a6d03bf3ce05214a1d
Fredrik Vedvik <75886490+fredrikved@users.noreply.github.com>
2024-01-29 08:52:21 +0100

    Update client.ts

--- src/services/hiddentreasures/client.ts
+++ src/services/hiddentreasures/client.ts
@@ -2,5 +2,6 @@ import { Client } from "hiddentreasures-js";
 import auth from "../auth";
 
 export default new Client({
+    basePath: "https://songtreasures.azurewebsites.net/",
     getToken: async () => (await auth.getToken()) ?? "",
 });
